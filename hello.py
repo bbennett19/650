24d508710b350f4ff7305d6ff51ba411788a0da3
bbennett19 <benbennett1991@gmail.com>
2017-10-11 20:48:06 +0000

    fixing the ctrl-c handler

--- hello.py
+++ hello.py
@@ -1,28 +1,28 @@
-import mraa, time
+import mraa, time, sys, signal
 
 def control_c_handler(signum, frame):
-	print('saw control-c')
-	print("Turning off all LEDS...")
-	for led in leds:
-		led.write(1)
-	print("Exiting")
-	sys.exit(0)
+    print('saw control-c')
+    print("Turning off all LEDS...")
+    for led in leds:
+        led.write(1)
+    print("Exiting")
+    sys.exit(0)
 
 signal.signal(signal.SIGINT, control_c_handler)
 
 leds = []
 for i in range(2,10):
-	led = mraa.Gpio(i)
-	led.dir(mraa.DIR_OUT)
-  if i%2 == 0:
-    leds.insert(0,led)
-  else:
-    leds.insert(len(leds),led)
+    led = mraa.Gpio(i)
+    led.dir(mraa.DIR_OUT)
+    if i%2 == 0:
+        leds.insert(0,led)
+    else:
+        leds.insert(len(leds),led)
 
 
 while True:
-  for led in leds:
-		led.write(0)
-		time.sleep(0.2)
-		led.write(1)
-		time.sleep(0.2)
+    for led in leds:
+        led.write(0)
+        time.sleep(0.2)
+        led.write(1)
+        time.sleep(0.2)
