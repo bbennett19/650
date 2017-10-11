2dcde57300a0713b21c94822cf46754b59f49890
Ben Bennett <bbennett@uoregon.edu>
2017-10-11 13:25:47 -0700

    updated pattern

--- hello.py
+++ hello.py
@@ -1,13 +1,16 @@
 import mraa, time
 leds = []
-for i in range(2,10):
-  led = mraa.Gpio(i)
-  led.dir(mraa.DIR_OUT)
-  leds.append(led)
+for i in range(2,10):led = mraa.Gpio(i)
+	led.dir(mraa.DIR_OUT)
+	if i%0:
+		leds.instert(0,led)
+	else:
+		leds.insert(len(leds),led)
+
 
 while True:
-  for led in leds:
-    led.write(0)
-    time.sleep(0.2)
-    led.write(1)
-    time.sleep(0.2)
+	for led in leds:
+		led.write(0)
+		time.sleep(0.2)
+		led.write(1)
+		time.sleep(0.2)
