0de176d7a63fe7b3a3b837c241e9e24e28dcb5de
bbennett19 <benbennett1991@gmail.com>
2017-10-15 23:28:50 +0000

    fixed weird tabbing issues

--- internet_connection_test_service/internet_connection_test.py
+++ internet_connection_test_service/internet_connection_test.py
@@ -7,17 +7,17 @@ leds = []
 for i in range(2,10):
     led = mraa.Gpio(i)
     led.dir(mraa.DIR_OUT)
-		leds.append(led)
-		led.write(1)
+    leds.append(led)
+    led.write(1)
 
 
 while True:
-	try:
-		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		s.connect(("8.8.8.8", 80))
-		leds[0].write(0)
-		leds[len(leds)-1].write(1)
-	except:
-		leds[0].write(1)
-		leds[len(leds)-1].write(0)
-	time.sleep(5)
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        leds[0].write(1)
+        leds[len(leds)-1].write(0)
+    except:
+        leds[0].write(0)
+        leds[len(leds)-1].write(1)
+    time.sleep(5)
